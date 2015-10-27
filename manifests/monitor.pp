94bed6eae583e4689fa966791b38d58ea3f260e0
Anthony Leach <anthony.leach@bigcommerce.com>
2015-10-27 07:43:01 -0700

    BIG-23190: add support for program checks

--- manifests/monitor.pp
+++ manifests/monitor.pp
@@ -6,6 +6,7 @@
 #
 # [*pidfile*]      - Path to the pid file for the service
 # [*matching*]     - String to match a process
+# [*program*]      - File to execute for check
 # [*ensure*]       - If the file should be enforced or not (default: present)
 # [*ip_port*]      - Port to check if needed (zero to disable)
 # [*socket*]       - Path to socket file if needed (undef to disable)
@@ -31,6 +32,7 @@
 define monit::monitor (
   $pidfile       = undef,
   $matching      = undef,
+  $program       = undef,
   $ensure        = present,
   $ip_port       = 0,
   $socket        = undef,
@@ -44,14 +46,14 @@ define monit::monitor (
   $gid           = '',
 ) {
   include monit::params
-  if ($pidfile == undef) and ($matching == undef) {
-    fail('Only one of pidfile and matching must be specified.')
+  if ($pidfile == undef) and ($matching == undef) and ($program == undef) {
+    fail('Only one of pidfile, matching or program must be specified.')
   }
-  if ($pidfile != undef) and ($matching != undef) {
-    fail('One of pidfile and matching must be specified.')
+  if ($pidfile != undef) and ($matching != undef) and ($program != undef) {
+    fail('Only one of pidfile, matching or program must be specified.')
   }
 
-  # Template uses: $pidfile, $ip_port, $socket, $checks, $start_script, $stop_script, $start_timeout, $stop_timeout, $group, $uid, $gid
+  # Template uses: $pidfile, $program, $ip_port, $socket, $checks, $start_script, $stop_script, $start_timeout, $stop_timeout, $group, $uid, $gid
   file { "${monit::params::conf_dir}/${name}.conf":
     ensure  => $ensure,
     content => template('monit/process.conf.erb'),
